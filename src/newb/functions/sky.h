84867141f746720b9dafd9934ccd344961e8cf55
goboyy-mc <lbtiktok200@gmail.com>
2026-08-04 22:07:27 +0700

    Update sky.h

--- src/newb/functions/sky.h
+++ src/newb/functions/sky.h
@@ -162,6 +162,46 @@ vec3 renderEndSky(vec3 horizonCol, vec3 zenithCol, vec3 viewDir, float t) {
 
   // 3. Efek Spektrum Aurora: Memberikan bias warna pelangi tipis (cyan-violet) di celah-celah kegelapan
   sky += 0.25 * streaks * spectrum(sin(2.2*viewDir.x*viewDir.y + t)) * vec3(0.3, 0.6, 1.5);
+  
+  // Author: devendrn, Title: Simple blackhole, License: CC BY-SA 4.0
+
+  #define NL_BH_COL_LOW vec3(0.10, 0.30, 0.90)
+  #define NL_BH_COL_HIGH vec3(0.10, 1.10, 2.20)
+  #define NL_BH_DIST 2.20
+  #define NL_BH_SPEED 0.18
+
+  vec4 renderBlackhole(vec3 vdir, float t) {
+    t *= NL_BH_SPEED;
+    
+    float r = 3.05;
+    //r += 0.1*t;
+    vec3 vr = vdir;
+    vr.xy = mat2(cos(r), -sin(r), sin(r), cos(r)) * vr.xy;
+    //r *= 2.0;
+    //vr.yz = mat2(cos(r), -sin(r), sin(r), cos(r)) * vr.yz;
+    
+    vec3 vd = vr-vec3(0.0, -1.0, 0.0);
+    float nl = sin(15.0*vd.x + t)*sin(15.0*vd.y - t)*sin(15.0*vd.z + t);
+    float a = atan(vd.x, vd.z);
+    
+    float d = NL_BH_DIST*length(vd + 0.003*nl);
+    //d *= 1.2 + 0.8*sin(0.2*t);
+    float d0 = (0.6-d)/0.6;
+    float dm0 = 1.0-max(d0, 0.0);
+    
+    float gl = 1.0-clamp(-0.3*d0, 0.0, 1.0);
+    float gla = pow(1.0-min(abs(d0), 1.0), 8.0);
+    float gl8 = pow(gl, 8.0);
+    
+    float hole = 0.9*pow(dm0, 32.0) + 0.1*pow(dm0, 3.0);
+    float bh = (gla + 0.8*gl8 + 0.2*gl8*gl8) * hole;
+    
+    float df = sin(3.0*a - 4.0*d + 24.0*pow(1.4-d, 4.0) + t);
+    df *= 0.9 + 0.1*sin(8.0*a + d + 4.0*t - 4.0*df);
+    bh *= 1.0 + pow(df, 4.0)*hole*max(1.0-bh, 0.0);
+    
+    vec3 col = bh*4.0*mix(NL_BH_COL_LOW, NL_BH_COL_HIGH , min(bh, 1.0));
+    return vec4(col, hole);
 
   return sky;
 }
